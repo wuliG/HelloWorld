24c48b4c051a5e64ad30a3f5dc8c4f4ea4932db6
Your Name <1094804299@qq.com>
2017-10-19 14:48:41 +0800

    新的测试

---
+++ src/cn/gao/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package cn.gao.demo;
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("helloWorld");
+    }
+    public void tst(){
+    System.out.println("test");
+    }
+}
